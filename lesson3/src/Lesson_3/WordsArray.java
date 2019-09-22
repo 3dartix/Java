8da2b018b03c4843a38021987f66ab0495db6293
3dartix <info@3dartix.ru>
2019-09-22 22:23:25 +0200

    в процессе

---
+++ lesson3/src/Lesson_3/WordsArray.java
@@ -0,0 +1,40 @@
+package Lesson_3;
+
+import java.util.*;
+
+public class WordsArray {
+    public static void main(String[] args) {
+        ArrayList<String> wordsArr = new ArrayList<String>();
+        wordsArr.add("слово1");
+        wordsArr.add("слово1");
+        wordsArr.add("слово2");
+        wordsArr.add("слово3");
+        wordsArr.add("слово5");
+        wordsArr.add("слово6");
+
+        GetCountUniqueWords(wordsArr);
+    }
+
+    public static void GetCountUniqueWords(ArrayList<String> arr){
+        int count = 0;
+        Iterator<String> iter = arr.iterator();
+
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        while (iter.hasNext()) {
+            String t = iter.next();
+
+            System.out.println(hm.containsKey(t));
+            if (hm.containsKey(t)){
+                int value = hm.get(t);
+                hm.put(t, value + 1);
+            } else {
+                hm.put(t, 1);
+            }
+            System.out.println(hm);
+            //if (arr.contains("слово1"))
+
+
+        }
+    }
+}
